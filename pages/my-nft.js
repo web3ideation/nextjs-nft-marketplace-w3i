86a1f02944f2ce17af91447f3b796414ce834afd
Niklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-08-10 13:54:04 +0200

    show own NFT

--- pages/my-nft.js
+++ pages/my-nft.js
@@ -1,56 +1,27 @@
-import { useMoralis } from "react-moralis"
-import { useState } from "react"
-import NFTBox from "../components/NFTBox"
-import networkMapping from "../constants/networkMapping.json"
-import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
-import { useQuery } from "@apollo/client"
+import { useMoralis } from "react-moralis";
+import { useState } from "react";
+import NFTBox from "../components/NFTBox";
+import networkMapping from "../constants/networkMapping.json";
+import GET_ACTIVE_ITEMS from "../constants/subgraphQueries";
+import { useQuery } from "@apollo/client";
 
 export default function Home() {
-  const { isWeb3Enabled, chainId, account } = useMoralis() //user wird nicht importiert oder woher bekommt man den?
-  const chainString = chainId ? parseInt(chainId).toString() : "31337"
-  const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
-  const { loading, data: listedNfts, error } = useQuery(GET_ACTIVE_ITEMS)
-  const [hasOwnNFT, setHasOwnNFT] = useState(); // Variable zur Verfolgung, ob du eigene NFTs hast
+  const { isWeb3Enabled, chainId, account } = useMoralis();
+  const chainString = chainId ? parseInt(chainId).toString() : "31337";
+  const marketplaceAddress = networkMapping[chainString].NftMarketplace[0];
+
+  const { loading, data: listedNfts, error } = useQuery(GET_ACTIVE_ITEMS);
+  const [hasOwnNFT, setHasOwnNFT] = useState(false);
 
   if (loading || !listedNfts) {
-    return <div>Loading</div>
+    return <div>Loading...</div>;
   }
 
   if (error) {
-    return <div>Error loading NFTs</div>
+    return <div>Error loading NFTs</div>;
   }
 
-  const handleNFTClick = (nft) => {
-    console.log(nft);
-    console.log(account)
-    const { price, nftAddress, tokenId, seller } = nft;
-
-    console.log(seller);
-    if (account === seller) {
-      console.log("User address is equal to seller address");
-    } else if (!account) {
-      console.log("No user address found");
-    } else {
-      console.log("User address is not equal to seller address");
-    }
-
-    if (seller === account) {
-      setHasOwnNFT(true);
-
-      return (
-        <NFTBox
-          price={price}
-          nftAddress={nftAddress}
-          tokenId={tokenId}
-          marketplaceAddress={marketplaceAddress}
-          seller={seller}
-          key={`${nftAddress}${tokenId}`}
-        />
-      );
-    }
-
-    return null;
-  };
+  const isOwnedByUser = (seller) => seller === account || seller === undefined;
 
   return (
     <div className="mt-10 mx-48 p-4 bg-blue-100 rounded-2xl">
@@ -59,15 +30,22 @@ export default function Home() {
         <div className="flex flex-wrap pb-4">
           {isWeb3Enabled && chainId ? (
             <>
-              {listedNfts.activeItems.map((nft) => handleNFTClick(nft))}
-              {!hasOwnNFT && (
-                <div>
-                  Go get some NFT for yourself!!!
-                </div>
+              {listedNfts.activeItems.map((nft) =>
+                isOwnedByUser(nft.seller) ? (
+                  <NFTBox
+                    key={`${nft.nftAddress}${nft.tokenId}`}
+                    price={nft.price}
+                    nftAddress={nft.nftAddress}
+                    tokenId={nft.tokenId}
+                    marketplaceAddress={marketplaceAddress}
+                    seller={nft.seller}
+                  />
+                ) : null
               )}
+              {hasOwnNFT && <div>Go get some NFTs for yourself!!!</div>}
             </>
           ) : (
-            <div>Web3 Currently Not Enabled</div>
+            <div>Web3 is currently not enabled</div>
           )}
         </div>
       </div>
