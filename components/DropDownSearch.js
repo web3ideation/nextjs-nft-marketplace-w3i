a8987e99cac4ab310e3697b3572d91255c7c2727
Niklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-08-10 13:57:36 +0200

    dropDownSearch cleanup

--- components/DropDownSearch.js
+++ components/DropDownSearch.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
-import styles from "../styles/Home.module.css";
+import styles from '../styles/Home.module.css';
 
 const DropDownSearch = ({ buttonText, options }) => {
+    // State to track whether the dropdown is open or closed
     const [isOpen, setIsOpen] = useState(false);
+
+    // Ref to the root element of the dropdown
     const dropdownRef = useRef(null);
 
-    function toggleMenu() {
+    // Function to toggle the dropdown menu
+    const toggleMenu = () => {
         setIsOpen(!isOpen);
-    }
+    };
 
-    // Schließe das Dropdown-Menü, wenn irgendwo auf der Seite geklickt wird
+    // Effect to close the dropdown when a click occurs outside
     useEffect(() => {
         const handleOutsideClick = (event) => {
             if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -17,22 +21,28 @@ const DropDownSearch = ({ buttonText, options }) => {
             }
         };
 
+        // Add event listener to the window
         window.addEventListener('click', handleOutsideClick);
 
+        // Cleanup by removing event listener when component unmounts
         return () => {
             window.removeEventListener('click', handleOutsideClick);
         };
     }, []);
 
     return (
-        <div className="bg-blue-200/40 flex flex-col mb-1 rounded-2xl overflow-hidden" ref={dropdownRef}>
-            <button className="hover:bg-blue-500 bg-blue-400 rounded-2xl shadow p-2 w-48" onClick={toggleMenu}>
+        <div className={`bg-blue-200/40 flex flex-col mb-1 rounded-2xl overflow-hidden`} ref={dropdownRef}>
+            <button
+                className={`hover:bg-blue-500 bg-blue-400 rounded-2xl shadow p-2 w-48`}
+                onClick={toggleMenu}
+            >
                 {buttonText}
             </button>
             {isOpen && (
                 <div className={styles.scrollbar}>
                     {options.map((option) => (
-                        <div className="hover:bg-blue-500 bg-blue-500/50 flex justify-center my-1 mr-1 p-2 rounded-2xl shadow"
+                        <div
+                            className={`hover:bg-blue-500 bg-blue-500/50 flex justify-center my-1 mr-1 p-2 rounded-2xl shadow`}
                             key={option.id}
                         >
                             {option.label}
