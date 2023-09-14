8aeb925135b1d1bfe68f8dbd2c52a0ab1d843e67
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-09-14 22:09:58 +0200

    handling and scrolling

--- pages/my-nft.js
+++ pages/my-nft.js
@@ -1,5 +1,5 @@
 import { useMoralis } from "react-moralis"
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import NFTBox from "../components/NFTBox"
 import networkMapping from "../constants/networkMapping.json"
 import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
@@ -22,24 +22,53 @@ export default function Home() {
         return <div>Error loading NFTs</div>
     }
 
+    const [isMouseWheelDisabled, setIsMouseWheelDisabled] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleNFTListedScroll = useCallback((event) => {
+    if (isModalOpen || isMouseWheelDisabled) {
+      return;
+    }
+    const container = document.getElementById('NFTListed');
+    if (container) {
+      container.scrollLeft += event.deltaY < 0 ? -226 : 226;
+    }
+  }, [isModalOpen, isMouseWheelDisabled]);
+
+  useEffect(() => {
+    const preventPageScroll = () => {
+      const container = document.getElementById('NFTListed');
+      if (container) {
+        container.addEventListener("wheel", (event) => {
+          event.preventDefault();
+        }, { passive: false });
+      }
+    };
+    preventPageScroll();
+  }, []);
+
     const isOwnedByUser = (seller) => seller === account || seller === undefined
 
     return (
         <div className={styles.NFTContainer}>
             <h1>My NFT</h1>
-            <div className={styles.NFTListed}>
+            <div className={styles.NFTListed} onWheel={handleNFTListedScroll}>
                 <div className="flex flex-wrap pb-4">
                     {isWeb3Enabled && chainId ? (
                         <>
                             {listedNfts.activeItems.map((nft) =>
                                 isOwnedByUser(nft.seller) ? (
                                     <NFTBox
-                                        key={`${nft.nftAddress}${nft.tokenId}`}
                                         price={nft.price}
                                         nftAddress={nft.nftAddress}
                                         tokenId={nft.tokenId}
                                         marketplaceAddress={marketplaceAddress}
                                         seller={nft.seller}
+                                        key={`${nft.nftAddress}${nft.tokenId}`}
+                                        disableMouseWheel={() => setIsMouseWheelDisabled(true)}
+                                        enableMouseWheel={() => setIsMouseWheelDisabled(false)}
+                                        anyModalIsOpen={() => setIsModalOpen(true)}
+                                        anyModalIsClosed={() => setIsModalOpen(false)}
                                     />
                                 ) : null
                             )}
