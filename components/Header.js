125505924b9b6a9c76e48ecc3213ccef17f14026
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-09-27 10:48:38 +0200

    searchBar improvements

--- components/Header.js
+++ components/Header.js
@@ -2,12 +2,10 @@ import { Button, ConnectButton } from "web3uikit"
 import Link from "next/link"
 import PopupMenu from "./PopupMenu"
 import SearchBar from "./SearchBar"
-import React, { useState } from "react"
+import React from "react"
 import styles from "../styles/Home.module.css"
 
 export default function Header({}) {
-    const [searchResults, setSearchResults] = useState([])
-
     return (
         <div className={styles.headerContainer}>
             <nav>
@@ -18,7 +16,7 @@ export default function Header({}) {
                     <h1 className={styles.headerHeadline}>NFT Marketplace</h1>
                 </div>
                 <div className={styles.headerElementsWrapper}>
-                    <SearchBar setSearchResults={searchResults} />
+                    <SearchBar />
                     <Link href="/my-nft" className={styles.headerButton}>
                         <Button text="Create" />
                     </Link>
