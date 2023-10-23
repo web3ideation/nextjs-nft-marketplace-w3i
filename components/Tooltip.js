033b14b933aa7407ac89e2f319b0342bd7752be9
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-10-23 15:38:56 +0200

    added tooltip for errorMsg when wrong value is giv

---
+++ components/Tooltip.js
@@ -0,0 +1,11 @@
+import styles from "../styles/Home.module.css"
+
+function Tooltip({ message }) {
+    return (
+        <div className={styles.tooltip}>
+            <span className={styles.tooltipText}>{message}</span>
+        </div>
+    )
+}
+
+export default Tooltip
