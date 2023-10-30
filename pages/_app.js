06e92faee1c04e7c91437d374cef3dbe1a9e5c35
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-10-30 13:58:59 +0100

    Revert "added comments and NFTProvider(for NFTSDATA)"
    This reverts commit 2dd828482f68772cb9af8110f6e19236b936b1dd.

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,21 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 import Head from "next/head"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
-import { NFTProvider } from "../components/NFTContextProvider"
 import { SearchResultsProvider } from "../components/SearchResultsContext"
 import Header from "../components/Header"
 import "../styles/globals.css"
 import React, { useEffect, useState } from "react"
 import LoadingIcon from "../public/LoadingIcon"
 import styles from "../styles/Home.module.css"
-import Footer from "../components/Footer"
 
-// Initialize Apollo Client
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL, // !!!W Note: This is centralized!
+    uri: process.env.NEXT_PUBLIC_SUBGRAPH_URL, // !!!W this is centralized!
 })
 
 function MyApp({ Component, pageProps }) {
-    // State for loading indication
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(true) // Responsible for loading
 
-    // Set a timeout to simulate loading
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(false)
@@ -39,27 +34,26 @@ function MyApp({ Component, pageProps }) {
                 <link rel="icon" href="/media/only-lightbulb-favicone.ico" />
                 <link rel="canonical" href="https://w3i-marketplace.com" />
             </Head>
-            {/* Wrap the app with necessary providers */}
             <MoralisProvider initializeOnMount={false}>
                 <ApolloProvider client={client}>
                     <NotificationProvider>
-                        <NFTProvider>
-                            <SearchResultsProvider>
-                                <Header />
-                                {/* Display loading icon or the component based on isLoading state */}
-                                {isLoading ? (
+                        <SearchResultsProvider>
+                            <Header />
+                            {isLoading ? (
+                                <div>
                                     <div className={styles.mainLoadingIconWrapper}>
                                         <LoadingIcon className={styles.mainLoadingIcon} />
                                     </div>
-                                ) : (
+                                </div>
+                            ) : (
+                                <>
                                     <Component {...pageProps} />
-                                )}
-                            </SearchResultsProvider>
-                        </NFTProvider>
+                                </>
+                            )}
+                        </SearchResultsProvider>
                     </NotificationProvider>
                 </ApolloProvider>
             </MoralisProvider>
-            <Footer />
         </div>
     )
 }
