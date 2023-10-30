d6075344ab2b27894e2e941fe968a03a111737c7
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-10-30 13:56:36 +0100

    NFTCollect. added SSideFilters remved upd states

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,13 @@
-import { useMoralis } from "react-moralis"
-import React, { useState, useCallback } from "react"
+import React from "react"
 import NFTListed from "../components/NFTListed"
-import SearchSideFilters from "../components/SearchSideFilters"
 import styles from "../styles/Home.module.css"
+import NFTCollection from "../components/NFTCollection"
 
 export default function Home() {
-    const { isWeb3Enabled, chainId } = useMoralis()
-
-    const [items, setItems] = useState([])
-    const [filteredItems, setFilteredItems] = useState([])
-
-    const handleFilteredItemsChange = useCallback((newFilteredItems) => {
-        setFilteredItems(newFilteredItems)
-    }, [])
-
     return (
         <div className={styles.nftListingContainer}>
-            <SearchSideFilters
-                initialItems={items}
-                onFilteredItemsChange={handleFilteredItemsChange}
-            />
-            <NFTListed
-                isWeb3Enabled={isWeb3Enabled}
-                chainId={chainId}
-                items={items}
-                setItems={setItems}
-                filteredItems={filteredItems}
-                setFilteredItems={setFilteredItems}
-            />
+            <NFTListed />
+            <NFTCollection />
         </div>
     )
 }
