ce857fed9a852e586d991080fb0f0a50b6fff7c4
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-11-02 23:23:27 +0100

    fixed last commit

--- components/NFTBox.js
+++ components/NFTBox.js
@@ -149,34 +149,43 @@ export default function NFTBox({ nftData, loadingImage }) {
             true
         )
 
-        buyItem({
-            onError: (error) => {
-                console.error(error)
-                clearNftNotification(notificationId)
-                setBuying(false)
-                showNftNotification("Error", "Could not complete the purchase.", "error", 6000)
-            },
-            onSuccess: async (tx) => {
-                console.log(tx)
-                // Achten Sie darauf, das Transaktionsobjekt zu erfassen
-                try {
-                    await tx.wait(1) // Warten Sie auf die Bestätigung der Transaktion
-                    clearNftNotification(notificationId) // Benachrichtigung löschen
-                    showNftNotification("Success", "Purchase successful!", "success", 6000)
-                } catch (error) {
-                    console.error("Error processing transaction success:", error)
+        if (isOwnedByUser) {
+            setShowInfoModal(true)
+            setBuying(false) // Stellen Sie sicher, dass Sie den Kaufstatus zurücksetzen.
+            clearNftNotification(notificationId)
+        } else {
+            await buyItem({
+                onError: (error) => {
+                    console.error(error)
                     clearNftNotification(notificationId)
-                    showNftNotification(
-                        "Transaction Error",
-                        "An error occurred while purchasing.",
-                        "error",
-                        6000
-                    )
-                } finally {
                     setBuying(false)
-                }
-            },
-        })
+                    showNftNotification("Error", "Could not complete the purchase.", "error", 6000)
+                },
+                onSuccess: () => {
+                    handleBuyItemSuccess(notificationId)
+                },
+            })
+        }
+    }
+
+    // Handler for successful item purchase
+    const handleBuyItemSuccess = async (notificationId) => {
+        try {
+            await tx.wait(1)
+            clearNftNotification(notificationId) // Clear the sticky notification using its ID
+            showNftNotification("Success", "Purchase successful!", "success", 6000)
+        } catch (error) {
+            console.error("Error processing transaction success:", error)
+            clearNftNotification(notificationId)
+            showNftNotification(
+                "Transaction Error",
+                "An error occurred while purchasing.",
+                "error",
+                6000
+            )
+        } finally {
+            setBuying(false)
+        }
     }
 
     // Handler for updating price button click
@@ -201,17 +210,7 @@ export default function NFTBox({ nftData, loadingImage }) {
         )
     }, [showUpdateListingModal, showInfoModal, showSellModal, showListModal])
 
-    //------------------ Handlers for nftAddress to copy ------------------
-
-    // Handler for mouse enter on NFT address
-    const handleMouseEnter = () => {
-        setIsCopying(true)
-    }
-
-    // Handler for mouse leave on NFT address
-    const handleMouseLeave = () => {
-        setIsCopying(false)
-    }
+    //------------------ Handler for nftAddress to copy ------------------
 
     // Handler to copy NFT address to clipboard
     const copyNftAddressToClipboard = async () => {
@@ -325,8 +324,6 @@ export default function NFTBox({ nftData, loadingImage }) {
                     price={ethers.utils.formatUnits(price, "ether")}
                     buyerCount={buyerCount}
                     handleBuyClick={handleBuyClick}
-                    handleMouseEnter={handleMouseEnter}
-                    handleMouseLeave={handleMouseLeave}
                     copyNftAddressToClipboard={copyNftAddressToClipboard}
                     closeModal={() => setShowInfoModal(false)}
                     showConnectMessage={showConnectMessage}
@@ -346,8 +343,6 @@ export default function NFTBox({ nftData, loadingImage }) {
                     price={ethers.utils.formatUnits(price, "ether")}
                     buyerCount={buyerCount}
                     handleUpdatePriceButtonClick={handleUpdatePriceButtonClick}
-                    handleMouseEnter={handleMouseEnter}
-                    handleMouseLeave={handleMouseLeave}
                     copyNftAddressToClipboard={copyNftAddressToClipboard}
                     closeModal={() => setShowSellModal(false)}
                 />
@@ -366,8 +361,6 @@ export default function NFTBox({ nftData, loadingImage }) {
                     price={ethers.utils.formatUnits(price, "ether")}
                     buyerCount={buyerCount}
                     handleListClick={handleListClick}
-                    handleMouseEnter={handleMouseEnter}
-                    handleMouseLeave={handleMouseLeave}
                     copyNftAddressToClipboard={copyNftAddressToClipboard}
                     closeModal={() => setShowListModal(false)}
                 />
