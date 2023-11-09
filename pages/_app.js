4007c2024e4cead4a5f1fb51e56964c76ad5f71a
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-11-09 14:57:40 +0100

    css imported over link

--- pages/_app.js
+++ pages/_app.js
@@ -40,6 +40,14 @@ function MyApp({ Component, pageProps }) {
                 <meta name="copyright" content="Web3Ideation" />
                 <link rel="icon" href="/media/only-lightbulb-favicone.ico" />
                 <link rel="canonical" href="https://w3i-marketplace.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+                    rel="stylesheet"
+                />
             </Head>
             {/* Wrap the entire application with context providers to manage global state */}
             <MoralisProvider initializeOnMount={false}>
