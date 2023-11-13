8b782ff2e2e65a49a61a87e39008266331405c3c
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2023-11-13 16:20:45 +0100

    collectionModal more styles and Information

--- components/NFTCollectionModal.js
+++ components/NFTCollectionModal.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useNFT } from "../context/NFTContextProvider"
 import NFTBox from "./NFTBox"
 import Modal from "../components/Modal"
+import { ethers } from "ethers"
 import styles from "../styles/Home.module.css"
 
 export default function NFTCollectionModal({ onClose, selectedCollection, nftCollections, type }) {
@@ -29,7 +30,6 @@ export default function NFTCollectionModal({ onClose, selectedCollection, nftCol
     // Filtern Sie nftsData basierend auf selectedNFTs
     return (
         <Modal isVisible={true} cancelText="Close" onCancel={onClose}>
-            {" "}
             <div className={styles.collectionModalContentWrapper}>
                 <h2>{selectedCollectionData.firstTokenName} Collection</h2>
                 <div className={styles.collectionModalContent}>
@@ -40,7 +40,29 @@ export default function NFTCollectionModal({ onClose, selectedCollection, nftCol
                                 nftData={filteredNftData}
                             />
                         </div>
-                    ))}{" "}
+                    ))}
+                </div>
+            </div>
+            <div className={styles.collectionModalTextWrapper}>
+                <div className={styles.modalText}>
+                    <div>
+                        <p>Items: </p>
+                        <strong>{selectedCollectionData.count}</strong>
+                    </div>
+                    <div>
+                        <p>Token-Id's: </p>
+                        <strong>{selectedCollectionData.tokenIds}</strong>
+                    </div>
+                    <div>
+                        <p>Volume:</p>
+                        <strong>
+                            {ethers.utils.formatUnits(
+                                selectedCollectionData.collectionPrice,
+                                "ether"
+                            )}{" "}
+                            ETH
+                        </strong>
+                    </div>
                 </div>
             </div>
         </Modal>
