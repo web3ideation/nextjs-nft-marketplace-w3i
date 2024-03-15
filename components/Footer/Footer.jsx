35ff9171928d530537d95773fcbcfbd7b3b8d3d1
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2024-03-15 14:19:33 +0100

    footer styling part 1

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,74 +1,89 @@
+import React from "react"
 import styles from "./Footer.module.scss"
+
+import BtnWithAction from "../uiComponents/BtnWithAction"
+import Link from "next/link"
+import Image from "next/image"
+
+const handleClick = () => {
+    window.location.href = "https://web3ideation.com/contact"
+}
 
 export default function Footer({}) {
     return (
-        <footer id="rs-footer" className={styles.Footer}>
-            <div className={styles.FooterTop}>
+        <footer className={styles.footer}>
+            <div className={styles.footerTop}>
                 <div className={styles.footerContainer}>
-                    <div className="row">
-                        <div className="col-lg-4 footer-0">
-                            <div className="footer-logo-wrap">
-                                <a href="https://web3ideation.com/" className="footer-top-logo">
-                                    <img
-                                        src="https://web3ideation.com/wp-content/uploads/2023/07/Logo-insconsolata-straightened-negative-e1690296974366.png"
-                                        alt="web3ideation"
+                    <div className={styles.row}>
+                        <div className={styles.footerOne}>
+                            <div className={styles.footerLogoWrapper}>
+                                <Link
+                                    href="https://web3ideation.com/"
+                                    className={styles.footerTopLogo}
+                                >
+                                    <Image
+                                        src="/media/Logo-w3i-marketplace.png"
+                                        alt="Logo-W3I-Market"
+                                        width={200}
+                                        height={100}
                                     />
-                                </a>
+                                </Link>
                             </div>
-                            <section
-                                id="custom_html-1"
-                                className="widget_text widget widget_custom_html"
-                            >
-                                <div className="textwidget custom-html-widget">
-                                    Marketplace @ web3ideation
-                                    <p className="footer-btn-wrap">
-                                        <a
-                                            href="https://web3ideation.com/contact"
-                                            className="footer-btn"
-                                        >
-                                            Kontakt
-                                        </a>
-                                    </p>
-                                    <br />
+                            <section className={styles.footerTextWrapper}>
+                                <div className={styles.footerText}>
+                                    Marketplace by web3ideation
+                                    <div className={styles.footerBtnWrapper}>
+                                        <BtnWithAction
+                                            onClickAction={handleClick}
+                                            buttonText={"Kontakt"}
+                                        ></BtnWithAction>
+                                    </div>
                                 </div>
-                            </section>{" "}
+                            </section>
                         </div>
-                        <div className="col-lg-4 footer-1">
-                            <section
-                                id="contact_widget-2"
-                                className="widget widget_contact_widget"
-                            >
-                                <h3 className="footer-title">Kontakt Info</h3>
-                                <ul className="fa-ul">
+                        <div className={styles.footerTwo}>
+                            <section className={styles.footerContactWrapper}>
+                                <h3 className={styles.footerTitle}>Kontakt Info</h3>
+                                <ul className={styles.contactList}>
                                     <li>
-                                        <i className="flaticon-call"></i>
-                                        <a href="tel:+491747440371"> +49 174 7440371</a>
-                                    </li>
-                                    <li>
-                                        <i className="flaticon-email"></i>
-                                        <a href="mailto:info@web3ideation.com">
+                                        <i className={styles.mailIcon}></i>
+                                        <Link
+                                            className={styles.footerLinks}
+                                            href="mailto:info@web3ideation.com"
+                                        >
                                             info@web3ideation.com
-                                        </a>
+                                        </Link>
                                     </li>
                                 </ul>
-                            </section>{" "}
+                            </section>
                         </div>
-                        <div className="col-lg-4 footer-3">
-                            <section id="text-3" className="widget widget_text">
-                                <h3 className="footer-title">Rechtliches</h3>{" "}
-                                <div className="textwidget">
+                        <div className={styles.footerThree}>
+                            <section id="text-3" className={styles.footerTextWrapper}>
+                                <h3 className={styles.footerTitle}>Rechtliches</h3>{" "}
+                                <div className={styles.footerText}>
                                     <p>
-                                        <a href="https://web3ideation.com/impressum">Impressum</a>
+                                        <Link
+                                            className={styles.footerLinks}
+                                            href="https://web3ideation.com/impressum"
+                                        >
+                                            Impressum
+                                        </Link>
                                     </p>
                                     <p>
-                                        <a href="https://web3ideation.com/datenschutzerklarung">
+                                        <Link
+                                            className={styles.footerLinks}
+                                            href="https://web3ideation.com/datenschutzerklarung"
+                                        >
                                             Datenschutzerklärung
-                                        </a>
+                                        </Link>
                                     </p>
                                     <p>
-                                        <a href="https://web3ideation.com/website-credits">
+                                        <Link
+                                            className={styles.footerLinks}
+                                            href="https://web3ideation.com/website-credits"
+                                        >
                                             Website Credits
-                                        </a>
+                                        </Link>
                                     </p>
                                     <p>© Copyright 2023 – web3ideation.com</p>
                                 </div>
