e10cd7244815257137909a69aa1cda732cc5c2aa
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2024-07-03 11:57:04 +0200

    proceeds loading optimized

--- pages/withdraw-proceeds.jsx
+++ pages/withdraw-proceeds.jsx
@@ -1,54 +1,48 @@
 import React, { useEffect, useState } from "react"
-
 import { ethers } from "ethers"
 import { useAccount, usePublicClient } from "wagmi"
-import { useWeb3Modal } from "@web3modal/wagmi/react"
-
-import { useGetProceeds } from "@hooks/useGetProceeds"
-import { useWithdrawProceeds } from "@hooks/useWithdrawProceeds"
+import { useGetProceeds } from "../hooks/index"
+import { useWithdrawProceeds } from "../hooks/index"
 import { fetchEthToEurRate } from "@utils/fetchEthToEurRate"
 import networkMapping from "@constants/networkMapping.json"
-
+import { useModal } from "@context/ModalProvider"
 import ConnectWalletBtn from "@components/Btn/ConnectWalletBtn/ConnectWalletBtn"
 import BtnWithAction from "@components/Btn/BtnWithAction"
-
 import styles from "@styles/Home.module.scss"
 
 const WithdrawProceeds = () => {
-    const [isClient, setIsClient] = useState(false)
     const [initialized, setInitialized] = useState(false)
     const provider = usePublicClient()
-    const { open } = useWeb3Modal()
     const chainId = provider.chains[0]
     const chainString = chainId.id ? parseInt(chainId.id).toString() : "31337"
     const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
-    const { address: userAdress, isConnected } = useAccount()
+    const { openModal, modalContent } = useModal()
+    const { address: userAddress, isConnected } = useAccount()
 
     const [proceeds, setProceeds] = useState("0.0")
     const [proceedsInEur, setProceedsInEur] = useState("0.0")
 
-    const handleWithdrawCompletion = () => {
-        refetchProceeds()
+    const { returnedProceeds, isLoadingProceeds, errorLoadingProceeds, proceedsStatus, refetchProceeds } =
+        useGetProceeds(marketplaceAddress, userAddress)
+
+    const handleWithdrawSuccess = () => {
+        openModal("withdrawn")
     }
 
-    const {
-        returnedProceeds,
-        isLoadingProceeds,
-        errorLoadingProceeds,
-        proceedsStatus,
-        refetchProceeds,
-    } = useGetProceeds(marketplaceAddress, userAdress)
+    const { handleWithdrawProceeds, isTxSuccess } = useWithdrawProceeds(marketplaceAddress, proceeds, refetchProceeds)
 
-    const { handleWithdrawProceeds, isWithdrawTxSuccess } = useWithdrawProceeds(
-        marketplaceAddress,
-        isConnected,
-        handleWithdrawCompletion
-    )
+    useEffect(() => {
+        if (userAddress) {
+            refetchProceeds()
+        }
+    }, [userAddress, refetchProceeds])
 
     useEffect(() => {
         if (returnedProceeds) {
             const proceedsInEther = ethers.utils.formatUnits(returnedProceeds.toString(), "ether")
             setProceeds(proceedsInEther)
+        } else {
+            setProceeds("0.0")
         }
     }, [returnedProceeds])
 
@@ -63,9 +57,8 @@ const WithdrawProceeds = () => {
     }, [proceeds])
 
     useEffect(() => {
-        setIsClient(typeof window !== "undefined")
         setInitialized(true)
-    }, [])
+    }, [initialized])
 
     if (!initialized) return null
 
@@ -77,24 +70,22 @@ const WithdrawProceeds = () => {
                 </div>
                 <div className={styles.withdrawProceedsInformation}>
                     <p>
-                        When selling or exchanging NFTs on our platform, it is important that you
-                        are clear about the withdrawal process of your proceeds. After a successful
-                        sale or exchange, your proceeds will be credited to your account in Ether
-                        (ETH). To access these funds, you will need to make a manual withdrawal,
-                        and please note that the withdrawal will be conducted only in ETH.
-                        Additionally, be aware that withdrawing funds will incur gas fees, which
-                        are required for processing the transaction on the Ethereum network. These
-                        fees vary based on network congestion. Therefore, we recommend planning
-                        your withdrawals accordingly. Please remember to withdraw your proceeds
-                        regularly to ensure your funds remain safe and accessible. This step is
-                        crucial to maintaining full control of your earned funds. If you need help
-                        or further information, do not hesitate to contact our support.
+                        When selling or exchanging NFTs on our platform, it is important that you are clear about the
+                        withdrawal process of your proceeds. After a successful sale or exchange, your proceeds will be
+                        credited to your account in Ether (ETH). To access these funds, you will need to make a manual
+                        withdrawal, and please note that the withdrawal will be conducted only in ETH. Additionally, be
+                        aware that withdrawing funds will incur gas fees, which are required for processing the
+                        transaction on the Ethereum network. These fees vary based on network congestion. Therefore, we
+                        recommend planning your withdrawals accordingly. Please remember to withdraw your proceeds
+                        regularly to ensure your funds remain safe and accessible. This step is crucial to maintaining
+                        full control of your earned funds. If you need help or further information, do not hesitate to
+                        contact our support.
                     </p>
                 </div>
                 {!isConnected ? (
                     <div>
                         Connect Wallet to show proceeds
-                        <ConnectWalletBtn onConnect={() => open()} isClient={isClient} />
+                        <ConnectWalletBtn />
                     </div>
                 ) : (
                     <>
@@ -114,7 +105,7 @@ const WithdrawProceeds = () => {
                             </div>
                         </div>
                         <div className={styles.withdrawProceedsBtn}>
-                            {isConnected & (proceeds !== "0.0") ? (
+                            {isConnected && proceeds !== "0.0" ? (
                                 <BtnWithAction
                                     buttonText={"Withdraw"}
                                     onClickAction={() => {
