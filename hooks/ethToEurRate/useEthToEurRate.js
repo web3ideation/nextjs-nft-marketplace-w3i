7614ac3216713adaafa846b3795213fdb31571db
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2024-09-05 14:38:08 +0200

    ethToEur hook optimization

--- hooks/ethToEurRate/useEthToEurRate.js
+++ hooks/ethToEurRate/useEthToEurRate.js
@@ -3,11 +3,25 @@ import { fetchEthToEurRate } from "@utils/fetchEthToEurRate"
 
 const useEthToEurRate = (pollingInterval = 60000) => {
     const [ethToEurRate, setEthToEurRate] = useState(null)
+    const [fetching, setFetching] = useState(false)
+    const [errorFetching, setErrorFetching] = useState(false)
+    const [fetchingSuccess, setFetchingSuccess] = useState(false)
 
     // Function to fetch and update the ETH to EUR rate
     const updateEthToEurRate = useCallback(async () => {
-        const rate = await fetchEthToEurRate()
-        setEthToEurRate(rate)
+        setFetching(true)
+        setErrorFetching(false)
+        try {
+            const rate = await fetchEthToEurRate()
+            setEthToEurRate(rate)
+            setFetchingSuccess(true)
+        } catch (error) {
+            console.error("Failed to fetch ETH to EUR rate:", error)
+            setErrorFetching(true)
+            setFetchingSuccess(false)
+        } finally {
+            setFetching(false)
+        }
     }, [])
 
     // Polling to update the exchange rate at the given interval
@@ -17,7 +31,7 @@ const useEthToEurRate = (pollingInterval = 60000) => {
         return () => clearInterval(intervalId) // Cleanup interval on unmount
     }, [updateEthToEurRate, pollingInterval])
 
-    return ethToEurRate
+    return { ethToEurRate, fetching, errorFetching, fetchingSuccess }
 }
 
 export default useEthToEurRate
