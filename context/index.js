a21abbcb6d8c16714152917ae27eab0da138aa23
NIklas Hoffmann <hoffmann.niklas@googlemail.com>
2024-10-17 17:29:35 +0200

    major refactoring / index added for context for better access

---
+++ context/index.js
@@ -0,0 +1,3 @@
+export { ModalProvider, useModal } from "./ModalProvider"
+export { NftProvider, useNFT } from "./NftDataProvider"
+export { NotificationProvider, useNotification } from "./NotificationProvider"
